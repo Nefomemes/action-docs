5d9b3f59182288f0a64d6f796d590c2650596717
Restyled.io <commits@restyled.io>
2021-02-25 07:33:33 +0000

    Restyled by shellharden

--- src/deploy.sh
+++ src/deploy.sh
@@ -2,15 +2,15 @@
 
 set -eo pipefail
 
-cd $GITHUB_WORKSPACE
+cd "$GITHUB_WORKSPACE"
 
 echo "::[notice] # Run the build"
 npm run docs
 
 # Initialise some useful variables
-REPO="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-BRANCH_OR_TAG=`awk -F/ '{print $2}' <<< $GITHUB_REF`
-CURRENT_BRANCH=`awk -F/ '{print $NF}' <<< $GITHUB_REF`
+REPO="https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
+BRANCH_OR_TAG=`awk -F/ '{print $2}' <<< "$GITHUB_REF"`
+CURRENT_BRANCH=`awk -F/ '{print $NF}' <<< "$GITHUB_REF"`
 
 if [ "$BRANCH_OR_TAG" == "heads" ]; then
   SOURCE_TYPE="branch"
@@ -20,15 +20,15 @@ fi
 
 echo "::[notice] # Checkout the repo in the target branch so we can build docs and push to it"
 TARGET_BRANCH="docs"
-git clone $REPO out -b $TARGET_BRANCH
+git clone "$REPO" out -b "$TARGET_BRANCH"
 
 cd out
 git pull
 echo "::[notice] # Move the generated JSON file to the newly-checked-out repo, to be committed and pushed"
-mv ../docs/docs.json $CURRENT_BRANCH.json
+mv ../docs/docs.json "$CURRENT_BRANCH".json
 echo "::[notice] # Commit and push"
 git add .
-git config user.name "${GITHUB_ACTOR}"
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-git commit -m "Docs build for ${SOURCE_TYPE} ${CURRENT_BRANCH}: ${GITHUB_SHA}" || true
-git push origin $TARGET_BRANCH
+git config user.name "$GITHUB_ACTOR"
+git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
+git commit -m "Docs build for $SOURCE_TYPE $CURRENT_BRANCH: $GITHUB_SHA" || true
+git push origin "$TARGET_BRANCH"
